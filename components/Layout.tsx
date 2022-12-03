6f8045900de813f9b2d396540641890c40783efb
nilsmanc <etereaiberia@gmail.com>
2022-12-03 09:53:40 +0300

    main layout

---
+++ components/Layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react'
+import Header from './Header'
+
+interface Props {
+  children?: ReactNode
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  )
+}
+
+export default Layout
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '../components/Layout'
+
+import styles from '../styles/App.module.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Layout>
+        <div className={styles.wrapper}>
+          <Component {...pageProps} />
+        </div>
+      </Layout>
+    </>
+  )
 }
